31493703dd11af765b22da758afc228eda58d710
Sam Joseph <tansaku@gmail.com>
2017-09-26 09:49:36 +0100

    added code of conduct

--- test/greeting_test.js
+++ test/greeting_test.js
@@ -1,18 +1,22 @@
-var chai = require('chai');
-var expect = chai.expect;
+var chai = require('chai')
+var expect = chai.expect
 
-var greeting = require('./../lib/greeting');
+var greeting = require('./../lib/greeting')
 
 describe('Bot', function(){
   it('should include linked techtalk channel in greeting',function(){
-    expect(greeting).to.include('<#C02AA0ARR|techtalk>');
+    expect(greeting).to.include('<#C02AA0ARR|techtalk>')
   });
 
   it('should include linked new_members channel in greeting',function(){
-    expect(greeting).to.include('<#C02G8J689|new_members>');
-  });
+    expect(greeting).to.include('<#C02G8J689|new_members>')
+  })
   
   it('should include linked random channel in greeting',function(){
-    expect(greeting).to.include('<#C0285CSUH|random>');
-  });
+    expect(greeting).to.include('<#C0285CSUH|random>')
+  })
+
+  it('should include link to our code of conduct',function(){
+    expect(greeting).to.include('<https://github.com/AgileVentures/AgileVentures/blob/master/CODE_OF_CONDUCT.md|code of conduct>')
+  })
 });
